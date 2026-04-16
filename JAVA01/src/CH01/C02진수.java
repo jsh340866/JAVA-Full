31bfc642472a74c340921593669a533579b3ee74
jsh340866 <jsh340866@gmail.com>
2026-04-16 17:20:18 +0900

    DAY 01 JAVA

--- JAVA01/src/CH01/C02진수.java
+++ JAVA01/src/CH01/C02진수.java
@@ -51,16 +51,25 @@ public class C02진수 {
 		// 179 -> 10110011
 		// 12  -> 00001100
 		// 15  -> 00001111
-
+							
 		// %d : 10진수 서식문자
 		// %o : 8진수 서식문자
 		// %x : 16진수 서식문자
 		// 코드 이쁘게 정리하기 : ctrl + shift + f
 		System.out.printf("10진수 : %d\n", 0b00001111);
-		System.out.printf("10진수 : %d\n", 173);
-		System.out.printf("10진수 : %d\n", 0255);
-		System.out.printf("10진수 : %d\n", 0XAD);
-
+		System.out.printf("10진수 : %d\n", 173);		//10진수
+		System.out.printf("10진수 : %d\n", 0255);	//8진수 (0  :8진수를 의미하는 접두사)
+		System.out.printf("10진수 : %d\n", 0XAD);	//16진수(0x :16진수를 의미하는 접두사)
+		
+		System.out.println();
+		System.out.printf("8진수 : %o\n", 173);		//10진수
+		System.out.printf("8진수 : %o\n", 0255);	//8진수 (0  :8진수를 의미하는 접두사)
+		System.out.printf("8진수 : %o\n", 0XAD);	//16진수(0x :16진수를 의미하는 접두사)
+		
+		System.out.println();
+		System.out.printf("16진수 : %x\n", 173);		//10진수
+		System.out.printf("16진수 : %x\n", 0255);	//8진수 (0  :8진수를 의미하는 접두사)
+		System.out.printf("16진수 : %x\n", 0XAD);	//16진수(0x :16진수를 의미하는 접두사)
 	}
 
 }
