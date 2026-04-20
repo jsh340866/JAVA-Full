9d1232734df1979fe3c713b999883af513ee1123
jsh340866 <jsh340866@gmail.com>
2026-04-20 17:32:17 +0900

    DAY 03 JAVA

--- JAVA01/src/CH03/C02Scanner.java
+++ JAVA01/src/CH03/C02Scanner.java
@@ -16,8 +16,12 @@ public class C02Scanner {
 		System.out.println("입력된 실수 값 : "+num2);
 		
 		System.out.println("문자열 입력 :");
-		String str1 = sc.next();		//문자열 입력 받기 함수, 띄어쓰기는 포함X
+//		String str1 = sc.next();		//문자열 입력 받기 함수, 띄어쓰기는 포함X
+//		System.out.println("입력된 문자열 : "+ str1);
 		
+		sc.nextLine();		// nextLine()을 한번더 써서 \n을 건너띄어 줘야한다
+		String str2 = sc.nextLine();		//문자열 입력 받기 함수, 띄어쓰기는 포함X
+		System.out.println("입력된 문자열 : "+ str2);
 		
 		sc.close();
 
