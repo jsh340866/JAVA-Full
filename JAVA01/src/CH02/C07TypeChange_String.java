0861f83a007c8fe0e1354a78c0f74a9f32a83662
jsh340866 <jsh340866@gmail.com>
2026-04-20 16:53:14 +0900

    DAY 03 JAVA

--- JAVA01/src/CH02/C07TypeChange_String.java
+++ JAVA01/src/CH02/C07TypeChange_String.java
@@ -17,22 +17,27 @@ public class C07TypeChange_String {
 		// 문자열 -> 숫자형 변환
 		//-----------------------------		
 		
-		//문자열 -> 숫자형으로 변환(정수)
+		//문자열 -> 숫자형으로 변환(정수) - Wrapper class 형으로 변환 뒤 처리
 //		int n1 = "10";
 		
-		System.out.println("10"+"20");		// 문자열 연결
-		int n1 = Integer.parseInt("10");	// 웹개발코드시 폼으로부터 전달받은 값은 다 문자열임
-		int n2 = Integer.parseInt("20");
-		System.out.println(n1+n2);
+//		System.out.println("10"+"20");		// 문자열 연결
+//		int n1 = Integer.parseInt("10");	// 웹개발코드시 폼으로부터 전달받은 값은 다 문자열임
+//		int n2 = Integer.parseInt("20");
+//		System.out.println(n1+n2);
+//		
+//		//문자열 -> 숫자형으로 변환(실수)
+//		double n3 = Double.parseDouble("10.5");
+//		double n4 = Double.parseDouble("20.4");
+//		System.out.println(n3+n4);
+//		
+//		short n5 = Short.parseShort("5");
+//		short n6 = Short.parseShort("6");
+//		System.out.println(n5+n6);
 		
-		//문자열 -> 숫자형으로 변환(실수)
-		double n3 = Double.parseDouble("10.5");
-		double n4 = Double.parseDouble("20.4");
-		System.out.println(n3+n4);
 		
-		short n5 = Short.parseShort("5");
-		short n6 = Short.parseShort("6");
-		System.out.println(n5+n6);
+		String strValue = "A";
+//		char var = (char)strValue;
+		char var = strValue.charAt(0);
 		
 		
 	}
---
+++ JAVA01/src/CH03/C01Scanner.java
@@ -0,0 +1,38 @@
+package CH03;
+
+import java.util.Scanner;
+
+// ctrl + shift + o // 필요한 import 자동으로 가져오기
+
+public class C01Scanner {
+
+	public static void main(String[] args) {
+		
+		//스트림 : 시냇물(단방향성)
+		//데이터의 논리적 이동채널(통로)
+		//System.in  : 표준 입력 스트림 요청 
+		//System.out : 표준 출력 스트림 요청
+		
+		//new Scanner(System.in) : 표준 입력 스트림을 연결한 Scanner객체
+		//Scanner sc = new Scanner(System.in)
+		// 표준 입력 스트림에 연결된 Scanner객체를 생성한 다음(생성위치 : HEAP)
+		// 해당 객체의 위치값(메모리주소)를 MainStack 영역의 Scanner sc 참조변수에 초기화
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("num1 입력 : ");
+		int num1 = sc.nextInt();	// 엔터(\n), 스페이스바
+		
+		System.out.println("num2 입력 : ");
+		int num2 = sc.nextInt();	// 엔터(\n), 스페이스바
+		
+		System.out.println("num3 입력 : ");
+		int num3 = sc.nextInt();	// 엔터(\n), 스페이스바
+		
+		
+		System.out.printf("%d %d %d\n", num1,num2,num3);
+		sc.close();
+		
+	}
+
+}
