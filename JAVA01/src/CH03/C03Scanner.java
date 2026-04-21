4534d4155e297c2561ea8b04c7a9fd64b03c0713
jsh340866 <jsh340866@gmail.com>
2026-04-21 09:21:27 +0900

    0421

--- JAVA01/src/CH03/C03Scanner.java
+++ JAVA01/src/CH03/C03Scanner.java
@@ -5,22 +5,31 @@ import java.util.Scanner;
 public class C03Scanner {
 
 	public static void main(String[] args) {
-		// 이름을 입력하세요? 홍길동
-		// 홍길동 님의 나이를 입력하세요? 35
-		// 홍길동 님의 주소를 입력하세요? 대구광역시 달서구 0000
-		// 홍길동 님의 나이는 35세 주소는 대구 광역시 ~~ 입니다
 		Scanner sc = new Scanner(System.in);
+		
+		// 이름을 입력하세요? 홍길동
 		System.out.printf("이름을 입력하세요? ");
 		String name = sc.nextLine();
 		
+		// 홍길동 님의 나이를 입력하세요? 35
 		System.out.printf("%s 님의 나이를 입력하세요? ",name);
 		int age = sc.nextInt();
-
+		
+		// 홍길동 님의 주소를 입력하세요? 대구광역시 달서구 0000
 		sc.nextLine();
 		System.out.printf("%s 님의 주소를 입력하세요? ",name);
 		String address = sc.nextLine();
 		
+		// 홍길동 님의 나이는 35세 주소는 대구 광역시 ~~ 입니다
 		System.out.println(name + " 님의 나이는 " + age + "세 주소는 " + address);
+		
+		
+
+
+
+		
+
+		
 	}
 
 }
---
+++ JAVA01/src/CH04/C01Basic.java
@@ -0,0 +1,10 @@
+package CH04;
+
+public class C01Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
