e0fccb4b842dd4ff27d751394bedb2c00f845674
jsh340866 <jsh340866@gmail.com>
2026-04-24 09:30:53 +0900

    0424

--- JAVA02/src/Ch01/C04MethodMain.java
+++ JAVA02/src/Ch01/C04MethodMain.java
@@ -1,38 +1,38 @@
-package Ch01;
-
-import java.util.Scanner;
-
-class Cal{
-	// 속성
-	Scanner sc = new Scanner(System.in);
-	
-	// 기능
-	public int sum1(int n1, int n2) {
-		return n1 +n2;
-	}
-	public int sum2() {
-		return sc.nextInt() + sc.nextInt();
-	}
-	public void sum3(int n1, int n2) {
-		System.out.println(n1+n2);
-	}
-	public void sum4() {
-		System.out.println(sc.nextInt() + sc.nextInt());
-	}
-}
-
-
-
-public class C04MethodMain {
-
-	public static void main(String[] args) {
-		Cal cal = new Cal();
-		System.out.println(cal.sum1(10, 20));
-		System.out.println(cal.sum2());
-		cal.sum3(10, 22);
-		cal.sum4();
-		
-
-	}
-
-}
+//package Ch01;
+//
+//import java.util.Scanner;
+//
+//class Cal{
+//	// 속성
+//	Scanner sc = new Scanner(System.in);
+//	
+//	// 기능
+//	public int sum1(int n1, int n2) {
+//		return n1 +n2;
+//	}
+//	public int sum2() {
+//		return sc.nextInt() + sc.nextInt();
+//	}
+//	public void sum3(int n1, int n2) {
+//		System.out.println(n1+n2);
+//	}
+//	public void sum4() {
+//		System.out.println(sc.nextInt() + sc.nextInt());
+//	}
+//}
+//
+//
+//
+//public class C04MethodMain {
+//
+//	public static void main(String[] args) {
+//		Cal cal = new Cal();
+//		System.out.println(cal.sum1(10, 20));
+//		System.out.println(cal.sum2());
+//		cal.sum3(10, 22);
+//		cal.sum4();
+//		
+//
+//	}
+//
+//}
