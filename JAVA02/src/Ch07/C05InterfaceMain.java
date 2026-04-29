4becd77130c9608de099cdd9323f17e808ef0bbb
jsh340866 <jsh340866@gmail.com>
2026-04-29 16:01:51 +0900

    0429

--- JAVA02/src/Ch07/C05InterfaceMain.java
+++ JAVA02/src/Ch07/C05InterfaceMain.java
@@ -1,10 +1,20 @@
 package Ch07;
 
+
+interface A {}
+interface B {}
+class C {}
+class D {}
+
+class E extends C implements A,B {
+	
+}
+
 public class C05InterfaceMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
---
+++ JAVA02/src/Ch07/C06Ex.java
@@ -0,0 +1,30 @@
+package Ch07;
+
+interface Printer {
+	void print();
+}
+class Inkjet implements Printer {
+
+	@Override
+	public void print() {
+		System.out.println("Inkjet 프린터 출력");
+		
+	}
+}
+class Laser extends Inkjet {
+	@Override
+	public void print() {
+		System.out.println("Laser 프린터 출력");
+		
+	}
+}
+
+public class C06Ex {
+	
+	public static void main(String[] args) {
+		Printer p = new Inkjet();
+		p.print();	// 'Inkjet 프린터 출력'
+		p = new Laser();
+		p.print();	// 'Laser 프린터 출력'
+	}
+}
