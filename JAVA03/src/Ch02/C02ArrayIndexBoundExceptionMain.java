db9d3cb97557b9a359558be0cde8f191086c44b8
jsh340866 <jsh340866@gmail.com>
2026-05-04 11:27:28 +0900

    0504

---
+++ JAVA03/src/Ch02/C02ArrayIndexBoundExceptionMain.java
@@ -0,0 +1,22 @@
+package Ch02;
+
+public class C02ArrayIndexBoundExceptionMain {
+
+	public static void main(String[] args) {
+		try {
+		int[] arr = { 10, 20, 30 };
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("예외처리 : " + e);
+			System.out.println("예외처리 : " + e.getStackTrace());
+			System.out.println("예외처리 : " + e.getCause());
+			
+		}
+		System.out.println("반드시 실행되어야 할 코드 1");
+		System.out.println("반드시 실행되어야 할 코드 2");
+	}
+
+}
