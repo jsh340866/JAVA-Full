51a9253f6396085ac067d66178c390ae00b02d48
jsh340866 <jsh340866@gmail.com>
2026-05-11 14:54:20 +0900

    0511

---
+++ JAVA03/src/Ch08/C01DBConn.java
@@ -0,0 +1,36 @@
+package Ch08;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class C01DBConn {
+
+	public static void main(String[] args) {
+
+		// DB CONN DATA
+		String id = "root";
+		String pw = "1234";
+		String url = "jdbc:mysql://localhost:3306/opendatadb";
+
+		// JDBC참조변수
+		Connection conn = null;			// DBMS의 특징 DB와 연결되는 객
+		PreparedStatement pstmt = null; // SQL Qyery 전송용 객체
+		ResultSet rs = null; 			// Select 결과물 담을 객체
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");		//
+			System.out.println("Driver Loading Success...");
+			conn = DriverManager.getConnection(url,id,pw);
+			System.out.println("DB CONNECTED..."); 
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {conn.close();}catch(Exception e2) {e2.printStackTrace();}
+		}
+		
+	}
+
+}
