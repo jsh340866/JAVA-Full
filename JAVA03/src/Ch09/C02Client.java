e5c0648600d5bdef6ddff115b4fda85e6e7669e3
jsh340866 <jsh340866@gmail.com>
2026-05-12 10:00:41 +0900

    0512

--- JAVA03/src/Ch09/C02Client.java
+++ JAVA03/src/Ch09/C02Client.java
@@ -1,10 +1,65 @@
 package Ch09;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class C02Client {
 
-	public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) throws IOException {
+		// 서버 생성
+		Socket server = new Socket("192.168.5.7", 7000);
+		System.out.println("[CLIENT] SERVER와의 연결이 완료되었습니다.");
+
+		// 기본스트림 // 바이트 단위로 받음
+		InputStream in = server.getInputStream();
+		OutputStream out = server.getOutputStream();
+
+		// 보조스트림 // int,double,String 등을 쓸 수 있게 해줌
+		DataInputStream din = new DataInputStream(in);
+		DataOutputStream dout = new DataOutputStream(out);
+
+		//
+		Scanner sc = new Scanner(System.in); // 키보드 입력
+		String send = null; // 송신용
+		String recv = null; // 수신용
+
+		// 루프
+		while (true) {
+			// Server -> Client(송신)
+			try {
+				recv = din.readUTF();
+			} catch (EOFException e) {
+				System.out.println("[ERROR] 클라이언트가 연결을 끊었습니다.");
+				break;
+			}
+			if (recv.equals("q"))
+				break;
+			System.out.println("[SERVER] : " + recv);
+			
+			
+			System.out.print("[CLIENT (q:종료)] : ");
+			send = sc.nextLine();
+			// Client -> Server(수신)
+			dout.writeUTF(send);
+			dout.flush();
+			if (send.equals("q"))
+				break;
+			
+
+		}
+		sc.close();
+		dout.close();
+		din.close();
+		out.close();
+		in.close();
+		server.close();
+		System.out.println("[CLIENT] 연결 종료합니다.");
 	}
-	
+
 }
--- JAVA03/src/Ch09/C02Server.java
+++ JAVA03/src/Ch09/C02Server.java
@@ -1,5 +1,70 @@
 package Ch09;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class C02Server {
+
+	public static void main(String[] args) throws IOException {
+		//서버 소켓 생성
+		ServerSocket server = new ServerSocket(7000); // IP: 서버IP(현재컴퓨터) PORT : 7000
+		System.out.println("[INFO] SERVER SOCKET LISTENING");
+		
+		//연결받는 작업
+		Socket client = server.accept();
+		System.out.println("[INFO] 접속자 IP : " + client.getInetAddress());
+		
+		//기본스트림 // 바이트 단위로 받음
+		InputStream in = client.getInputStream();
+		OutputStream out = client.getOutputStream();
+		
+		//보조스트림 // int,double,String 등을 쓸 수 있게 해줌
+		DataInputStream din = new DataInputStream(in);
+		DataOutputStream dout = new DataOutputStream(out);
+		
+		//
+		Scanner sc = new Scanner(System.in);	//키보드 입력
+		String send = null;		//송신용
+		String recv = null;		//수신용
+		
+		//루프
+		while(true) {
+			System.out.print("[SERVER (q:종료)] : ");
+			send = sc.nextLine();
+			//Server -> Client(송신)
+			dout.writeUTF(send);
+			dout.flush();
+			if(send.equals("q"))
+				break;
+			//Client -> Server(수신)
+			try {
+				recv = din.readUTF();
+			} catch(EOFException e) {
+				System.out.println("[ERROR] 클라이언트가 연결을 끊었습니다.");
+				break;
+			}
+			if(recv.equals("q"))
+				break;
+			System.out.println("[CLIENT] : " + recv);
+			
+		}
+		sc.close();
+		dout.close();
+		din.close();
+		out.close();
+		in.close();
+		client.close();
+		server.close();
+		System.out.println("[SERVER] 연결 종료합니다.");
+		
+		
+	}
 
 }
