ee6441dbef46a2aeef0c560eececc85a7eb35b3a
jsh340866 <jsh340866@gmail.com>
2026-05-12 10:28:49 +0900

    0512

--- JAVA03/src/Ch10/C02Worker2.java
+++ JAVA03/src/Ch10/C02Worker2.java
@@ -5,7 +5,7 @@ public class C02Worker2 implements Runnable {
 	@Override
 	public void run() {
 		
-		for(int i =0; i<5; i++) {
+		for(int i = 0; i<5; i++) {
 			System.out.println("TASK02...");
 			try {
 				Thread.sleep(500);
