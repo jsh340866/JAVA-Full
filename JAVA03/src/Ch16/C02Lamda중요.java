0fb2bac06c365c287345d6065fe26415ec2859ec
jsh340866 <jsh340866@gmail.com>
2026-05-13 10:57:07 +0900

    0513

--- JAVA03/src/Ch16/C02Lamda중요.java
+++ JAVA03/src/Ch16/C02Lamda중요.java
@@ -10,7 +10,9 @@ interface Printer {
 public class C02Lamda중요 {
 
 	public static void main(String[] args) {
-		// 01	// 인터페이스를 바로 연결하고 완성하는게 람다식, 단 하나의 메서드만 있어야함
+		// 01	
+		// 인터페이스를 바로 연결해 객체를 만들고 추상메서드를 완성하는게 람다식
+		// 단 인터페이스에 하나의 메서드만 있어야함
 //		Printer printer = (name)->{System.out.println("01 " + name);};
 //		Printer printer = System.out::println;
 //		printer.print("HELLO WORLD");
